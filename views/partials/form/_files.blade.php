e5802801e40dc28afe56132313a545a58f42743f
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-06-03 18:09:41 +0200

    files-translation

--- views/partials/form/_files.blade.php
+++ views/partials/form/_files.blade.php
@@ -3,26 +3,29 @@
     $max = $max ?? 1;
     $note = $note ?? 'Add' . ($max > 1 ? " up to $max files" : ' one file');
     $itemLabel = $itemLabel ?? strtolower($label);
+    $addLabelValue = $addLabelValue ?? 'Add';
 @endphp
 
 <a17-locale
     type="a17-filefield"
     :attributes="{
-        type: '{{ $type }}',
         label: '{{ $label }}',
         itemLabel: '{{ $itemLabel }}',
+        addLabelValue: '{{$addLabelValue}}',
         @include('twill::partials.form.utils._field_name', ['asAttributes' => true])
         note: '{{ $note }}',
-        max: {{ $max }}
-    }"
-></a17-locale>
+        max: {{ $max }},
+        }"
+>
+</a17-locale>
 
 @unless($renderForBlocks)
-@push('vuexStore')
-    @foreach(getLocales() as $locale)
-        @if (isset($form_fields['files']) && isset($form_fields['files'][$locale][$name]))
-            window.STORE.medias.selected["{{ $name }}[{{ $locale }}]"] = {!! json_encode($form_fields['files'][$locale][$name]) !!}
-        @endif
-    @endforeach
-@endpush
+    @push('vuexStore')
+        @foreach(getLocales() as $locale)
+            @if (isset($form_fields['files']) && isset($form_fields['files'][$locale][$name]))
+                window.STORE.medias.selected["{{ $name }}[{{ $locale }}]"] = {!! json_encode($form_fields['files'][$locale][$name]) !!}
+            @endif
+        @endforeach
+    @endpush
 @endunless
+
