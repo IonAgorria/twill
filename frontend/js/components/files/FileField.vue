cbfee7cb2afe5b7534daf0beb00ec1824710a9f9
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-06-03 17:58:08 +0200

    Update FileField.vue

--- frontend/js/components/files/FileField.vue
+++ frontend/js/components/files/FileField.vue
@@ -100,7 +100,7 @@
         }
       },
       addLabel: function () {
-        return this.addLabelValue + ' ' +this.itemLabel
+        return this.addLabelValue + ' ' + this.itemLabel
       },
       ...mapState({
         selectedFiles: state => state.mediaLibrary.selected
