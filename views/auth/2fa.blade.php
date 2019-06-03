18372826dd7882465d2b759e0a06eb7227b35bf4
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-06-03 14:04:33 +0200

    2fa translations solution

--- views/auth/2fa.blade.php
+++ views/auth/2fa.blade.php
@@ -1,13 +1,13 @@
 @extends('twill::auth.layout', [
     'route' => route('admin.login-2fa'),
-    'screenTitle' => 'Verify login'
+    'screenTitle' => __('auth.verifylogin')=='auth.verifylogin'?'Verify login':__('auth.verifylogin')
 ])
 
 @section('form')
     <fieldset class="login__fieldset">
-        <label class="login__label" for="verify-code">One-time password</label>
-        <input type="number" name="verify-code" class="login__input" required autofocus tabindex="1" />
+        <label class="login__label"
+               for="verify-code">{{__('auth.onetimepass')=='auth.onetimepass'?'One-time password':__('auth.onetimepass')}}</label>
+        <input type="number" name="verify-code" class="login__input" required autofocus tabindex="1"/>
     </fieldset>
-
     <input class="login__button" type="submit" value="Login" tabindex="3">
 @stop
