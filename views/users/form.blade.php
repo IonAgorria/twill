7ad3fef195b2465c24d08c8a7609c062a7843c12
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-06-06 10:42:13 +0200

    user form translations solution

--- views/users/form.blade.php
+++ views/users/form.blade.php
@@ -1,53 +1,73 @@
 @extends('twill::layouts.form', [
-    'contentFieldsetLabel' => 'User settings',
-    'editModalTitle' => 'Edit user name',
+    'contentFieldsetLabel' => __('users.usersettings') == 'users.usersettings' ? 'User settings' : __('users.usersettings'),
+    'editModalTitle' =>  __('users.editusername') == 'users.editusername' ?  'Edit user name': __('users.editusername'),
     'reloadOnSuccess' => true
 ])
 
 @php
     $isSuperAdmin = isset($item->role) ? $item->role === 'SUPERADMIN' : false;
+    $roleLabel = __('users.role') == 'users.role' ? 'Role' : __('users.role');
+    $rolePlaceHolder = __('users.selectrole') == 'users.selectrole' ? 'Select a role' : __('users.selectrole');
+    $profileImgLabel = __('users.profileimg') == 'users.profileimg' ? 'Profile image' : __('users.profileimg');
+    $titleLabel = __('users.title') == 'users.title' ? 'Title' : __('users.title');
+    $descriptionLabel = __('users.description') == 'users.description' ? 'User description' : __('users.description');
+    $twoFactorLabel = __('users.twofactor') == 'users.twofactor' ? '2-factor authentication' : __('users.twofactor');
+    $googleAuthLabel = __('users.googleauth') == 'users.googleauth' ? 'Please scan this QR code with a Google Authenticator compatible application and enter your one time password below before submitting. See a list of compatible applications ' : __('users.googleauth');
+    $hereLabel = __('users.here') == 'users.here' ? ' here' : __('users.here');
+    $oneTimePassLabel = __('users.onetimepass') == 'users.onetimepass' ? 'One time password' : __('users.onetimepass');
+    $twoFADisableLabel = __('users.twofactordisable') == 'users.twofactordisable' ? 'Enter your one time password to disable the 2-factor authentication' : __('users.twofactordisable');
+    $updateDisabledLabel = __('users.updatedisabled') == 'users.updatedisabled' ? 'Update disabled user' : __('users.updatedisabled');
+    $updateDisabledCloseLabel = __('users.updatedisabledclose') == 'users.updatedisabledclose' ? 'Update disabled and close' : __('users.updatedisabledclose');
+    $updateDisabledCreateLabel = __('users.updatedisablednew') == 'users.updatedisablednew' ? 'Update disabled user and create new' : __('users.updatedisablednew');
+    $cancelLabel = __('navigation.cancel') == 'navigation.cancel' ? 'Cancel' : __('navigation.cancel');
+    $enableUserLabel = __('users.enableuser') == 'users.enableuser' ? 'Enable user' : __('users.enableuser');
+    $enableUserCloseLabel = __('users.enableclose') == 'users.enableclose' ? 'Enable user and close' : __('users.enableclose');
+    $enableUserCreateLabel = __('users.enablecreate') == 'users.enablecreate' ? 'Enable user and create new' : __('users.enablecreate');
+    $updateLabel = __('navigation.update') == 'navigation.update' ? 'Update' : __('navigation.update');
+    $updateCloseLabel = __('users.updateclose') == 'users.updateclose' ? 'Update and close' : __('users.updateclose');
+    $updateNewLabel = __('users.updatecreate') == 'users.updatecreate' ? 'Update and create new' : __('users.updatecreate');
 @endphp
 
 @section('contentFields')
     @formField('input', [
-        'name' => 'email',
-        'label' => 'Email'
+    'name' => 'email',
+    'label' => 'Email'
     ])
 
     @can('edit-user-role')
         @if(!$isSuperAdmin && ($item->id !== $currentUser->id))
             @formField('select', [
-                'name' => "role",
-                'label' => "Role",
-                'options' => $roleList,
-                'placeholder' => 'Select a role'
+            'name' => "role",
+            'label' => "{{$roleLabel}}",
+            'options' => $roleList,
+            'placeholder' => '{{$rolePlaceHolder}}'
             ])
         @endif
     @endcan
 
     @if(config('twill.enabled.users-image'))
         @formField('medias', [
-            'name' => 'profile',
-            'label' => 'Profile image'
+        'name' => 'profile',
+        'label' => '{{$profileImgLabel}}'
         ])
     @endif
     @if(config('twill.enabled.users-description'))
         @formField('input', [
-            'name' => 'title',
-            'label' => 'Title',
-            'maxlength' => 250
+        'name' => 'title',
+        'label' => '{{$titleLabel}}',
+        'maxlength' => 250
         ])
         @formField('input', [
-            'name' => 'description',
-            'rows' => 4,
-            'type' => 'textarea',
-            'label' => 'Description'
+        'name' => 'description',
+        'rows' => 4,
+        'type' => 'textarea',
+        'label' => '{{$descriptionLabel}}'
         ])
     @endif
     @if($with2faSettings ?? false)
         @formField('checkbox', [
-            'name' => 'google_2fa_enabled',
-            'label' => '2-factor authentication',
+        'name' => 'google_2fa_enabled',
+        'label' => '{{$twoFactorLabel}}',
         ])
 
         @unless($item->google_2fa_enabled ?? false)
@@ -56,10 +76,13 @@
                 'fieldValues' => true,
             ])
                 <img style="display: block; margin-left: auto; margin-right: auto;" src="{{ $qrCode }}">
-                <div class="f--regular f--note" style="margin: 20px 0;">Please scan this QR code with a Google Authenticator compatible application and enter your one time password below before submitting. See a list of compatible applications <a href="https://github.com/antonioribeiro/google2fa#google-authenticator-apps" target="_blank" rel="noopener">here</a>.</div>
+                <div class="f--regular f--note" style="margin: 20px 0;">{{$googleAuthLabel}}<a
+                        href="https://github.com/antonioribeiro/google2fa#google-authenticator-apps" target="_blank"
+                        rel="noopener">{{$hereLabel}}</a>.
+                </div>
                 @formField('input', [
-                    'name' => 'verify-code',
-                    'label' => 'One time password',
+                'name' => 'verify-code',
+                'label' => '{{$oneTimePassLabel}}',
                 ])
             @endcomponent
         @else
@@ -68,9 +91,9 @@
                 'fieldValues' => false,
             ])
                 @formField('input', [
-                    'name' => 'verify-code',
-                    'label' => 'One time password',
-                    'note' => 'Enter your one time password to disable the 2-factor authentication'
+                'name' => 'verify-code',
+                'label' => '{{$oneTimePassLabel}}',
+                'note' => '{{$twoFADisableLabel}}'
 
                 ])
             @endcomponent
@@ -80,61 +103,61 @@
 
 @push('vuexStore')
     window.STORE.publication.submitOptions = {
-        draft: [
-          {
-            name: 'save',
-            text: 'Update disabled user'
-          },
-          {
-            name: 'save-close',
-            text: 'Update disabled and close'
-          },
-          {
-            name: 'save-new',
-            text: 'Update disabled user and create new'
-          },
-          {
-            name: 'cancel',
-            text: 'Cancel'
-          }
-        ],
-        live: [
-          {
-            name: 'publish',
-            text: 'Enable user'
-          },
-          {
-            name: 'publish-close',
-            text: 'Enable user and close'
-          },
-          {
-            name: 'publish-new',
-            text: 'Enable user and create new'
-          },
-          {
-            name: 'cancel',
-            text: 'Cancel'
-          }
-        ],
-        update: [
-          {
-            name: 'update',
-            text: 'Update'
-          },
-          {
-            name: 'update-close',
-            text: 'Update and close'
-          },
-          {
-            name: 'update-new',
-            text: 'Update and create new'
-          },
-          {
-            name: 'cancel',
-            text: 'Cancel'
-          }
-        ]
-      }
+    draft: [
+    {
+    name: 'save',
+    text: '{{$updateDisabledLabel}}'
+    },
+    {
+    name: 'save-close',
+    text: '{{$updateDisabledCloseLabel}}'
+    },
+    {
+    name: 'save-new',
+    text: '{{$updateDisabledCreateLabel}}'
+    },
+    {
+    name: 'cancel',
+    text: '{{$cancelLabel}}'
+    }
+    ],
+    live: [
+    {
+    name: 'publish',
+    text: '{{$enableUserLabel}}'
+    },
+    {
+    name: 'publish-close',
+    text: '{{$enableUserCloseLabel}}'
+    },
+    {
+    name: 'publish-new',
+    text: '{{$enableUserCreateLabel}}'
+    },
+    {
+    name: 'cancel',
+    text: '{{$cancelLabel}}'
+    }
+    ],
+    update: [
+    {
+    name: 'update',
+    text: '{{$updateLabel}}'
+    },
+    {
+    name: 'update-close',
+    text: '{{$updateCloseLabel}}'
+    },
+    {
+    name: 'update-new',
+    text: '{{$updateNewLabel}}'
+    },
+    {
+    name: 'cancel',
+    text: '{{$cancelLabel}}'
+    }
+    ]
+    }
     @if ($item->id == $currentUser->id)
         window.STORE.publication.withPublicationToggle = false
     @endif
