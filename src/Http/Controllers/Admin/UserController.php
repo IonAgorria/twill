290942b434053cf11db1df170012778d08c19aed
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-06-06 13:34:00 +0200

    UserController string translations

--- src/Http/Controllers/Admin/UserController.php
+++ src/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace A17\Twill\Http\Controllers\Admin;
 
 use A17\Twill\Models\Enums\UserRole;
+use Illuminate\Support\Facades\Log;
 use Auth;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
@@ -10,6 +11,7 @@ use PragmaRX\Google2FAQRCode\Google2FA;
 
 class UserController extends ModuleController
 {
+
     protected $namespace = 'A17\Twill';
 
     protected $moduleName = 'users';
@@ -54,6 +56,35 @@ class UserController extends ModuleController
         'role' => 'edit-user-role',
     ];
 
+    function mapStrings($key){
+        switch($key){
+            case 'name':
+                return __('users.name') == 'users.name' ? 'Name' : __('users.name');
+            case 'role':
+                return __('users.role') == 'users.role' ? 'Role' : __('users.role');
+            case 'image':
+                return __('users.image') == 'users.image' ? 'Image' : __('users.image');
+            case 'users':
+                return __('users.users') == 'users.users' ? 'Users' : __('users.users');
+            case 'enabled':
+                return __('users.enabled') == 'users.enabled' ? 'Enabled' : __('users.enabled');
+            case 'disabled':
+                return __('users.disabled') == 'users.disabled' ? 'Disabled' : __('users.disabled');
+            case 'active':
+                return __('users.active') == 'users.active' ? 'Active' : __('users.active');
+            case 'trash':
+                return __('users.trash') == 'users.trash' ? 'Trash' : __('users.trash');
+        }
+    }
+
+    function redoIndexCols(){
+        $this->indexColumns['name']['title'] = $this->mapStrings('name');
+        $this->indexColumns['role_value']['title'] = $this->mapStrings('role');
+        if(config('twill.enabled.users-image')){
+            $this->indexColumns['image']['title'] = $this->mapStrings('image');
+        }
+    }
+
     public function __construct(Application $app, Request $request)
     {
         parent::__construct($app, $request);
@@ -76,27 +107,34 @@ class UserController extends ModuleController
                 ],
             ] + $this->indexColumns;
         }
+        $this->redoIndexCols();
     }
 
     protected function indexData($request)
     {
+        $enabled = $this->mapStrings('enabled');
+        $disabled = $this->mapStrings('disabled');
+        $users = $this->mapStrings('users');
         return [
             'defaultFilterSlug' => 'published',
             'create' => $this->getIndexOption('create') && auth('twill_users')->user()->can('edit-user-role'),
             'roleList' => collect(UserRole::toArray()),
             'single_primary_nav' => [
                 'users' => [
-                    'title' => 'Users',
+                    'title' => $users,
                     'module' => true,
                 ],
             ],
-            'customPublishedLabel' => 'Enabled',
-            'customDraftLabel' => 'Disabled',
+            'customPublishedLabel' => $enabled,
+            'customDraftLabel' => $disabled,
         ];
     }
 
     protected function formData($request)
     {
+        $users = $this->mapStrings('users');
+        $enabled = $this->mapStrings('enabled');
+        $disabled = $this->mapStrings('disabled');
         $user = Auth::guard('twill_users')->user();
         $with2faSettings = config('twill.enabled.users-2fa') && $user->id == request('user');
 
@@ -121,12 +159,12 @@ class UserController extends ModuleController
             'roleList' => collect(UserRole::toArray()),
             'single_primary_nav' => [
                 'users' => [
-                    'title' => 'Users',
+                    'title' => $users,
                     'module' => true,
                 ],
             ],
-            'customPublishedLabel' => 'Enabled',
-            'customDraftLabel' => 'Disabled',
+            'customPublishedLabel' => $enabled,
+            'customDraftLabel' => $disabled,
             'with2faSettings' => $with2faSettings,
             'qrCode' => $qrCode ?? null,
         ];
@@ -139,21 +177,24 @@ class UserController extends ModuleController
 
     public function getIndexTableMainFilters($items, $scopes = [])
     {
+        $active = $this->mapStrings('active');
+        $disabled = $this->mapStrings('disabled');
+        $trash = $this->mapStrings('trash');
         $statusFilters = [];
 
         array_push($statusFilters, [
-            'name' => 'Active',
+            'name' => $active,
             'slug' => 'published',
             'number' => $this->repository->getCountByStatusSlug('published'),
         ], [
-            'name' => 'Disabled',
+            'name' => $disabled,
             'slug' => 'draft',
             'number' => $this->repository->getCountByStatusSlug('draft'),
         ]);
 
         if ($this->getIndexOption('restore')) {
             array_push($statusFilters, [
-                'name' => 'Trash',
+                'name' => $trash,
                 'slug' => 'trash',
                 'number' => $this->repository->getCountByStatusSlug('trash'),
             ]);
