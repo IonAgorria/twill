0ff01f12f1b4af1826fcfe312686e6497bf203a0
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-07-30 12:56:09 +0200

    create translations

--- views/partials/create.blade.php
+++ views/partials/create.blade.php
@@ -1,6 +1,6 @@
 @formField('input', [
     'name' => $titleFormKey ?? 'title',
-    'label' => ucfirst($titleFormKey ?? 'title'),
+    'label' => ucfirst(__('fields.'.$titleFormKey) == 'fields.'.$titleFormKey ? null : __('fields.'.$titleFormKey) ?? 'title'),
     'translated' => $translateTitle ?? false,
     'required' => true,
     'onChange' => 'formatPermalink'
