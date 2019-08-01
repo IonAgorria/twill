14ca71563056e150ee7f114d336bad546800f2e8
Tomás Cardenal <39330280+tomascardenal@users.noreply.github.com>
2019-08-01 10:57:17 +0200

    Update LangSwitcher.vue

--- frontend/js/components/LangSwitcher.vue
+++ frontend/js/components/LangSwitcher.vue
@@ -2,7 +2,7 @@
   <div class="language"
        :class="languageClass"
        v-if="languages.length > 1">
-    <span class="language__label f--small" v-if="!inModal">Edit in</span>
+    <span class="language__label f--small" v-if="!inModal">{{editInLabel}}</span>
     <span class="language__toolbar">
       <button type="button" class="language__button" :key="language.value" v-for="language in languages"
               :class="{ 'selected': language.value === localeValue.value, 'published': allPublished ? true : language.published }"
@@ -31,6 +31,10 @@
       allPublished: {
         type: Boolean,
         default: false
+      },
+      editInLabel: {
+        type: String,
+        default: 'Edit in'
       }
     },
     computed: {
