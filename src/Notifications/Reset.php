be2b816296d2b6e7d3fa65f7fc5ba1b9093db0ee
Ion Agorria <ion@agorria.com>
2019-12-19 14:58:35 +0100

    Esp email

--- src/Notifications/Reset.php
+++ src/Notifications/Reset.php
@@ -10,9 +10,9 @@ class Reset extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)->markdown('twill::emails.html.email', [
-            'url' => url('http://' . config('twill.admin_app_url') . route('admin.password.reset.form', $this->token, false)),
-            'actionText' => 'Reset password',
-            'copy' => 'You are receiving this email because we received a password reset. If you did not request a password reset, no further action is required.',
+            'url' => url('https://' . config('twill.admin_app_url') . route('admin.password.reset.form', $this->token, false)),
+            'actionText' => 'Restablecer la contraseña',
+            'copy' => 'Has recibido este correo porque estás creando una nueva contraseña para tu cuenta o porque la has reseteado o la quieres recuperar. Si no has sido tu simplemente elimina el correo, y si has sido tú haz click en el siguiente botón.',
         ]);
     }
 }
